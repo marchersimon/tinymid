7f777b2fa17f9808541bf4e4ae2e2e659b2682a3
marchersimon <marchersimon@zohomail.eu>
2021-06-20 11:32:28 +0200

    Add command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <getopt.h>
 
 #include "midfile.h"
 
+void printHelp() {
+	std::cout << "Usage: mid2icl [options] [file]\n"
+				 "\n"
+				 "Options:\n"
+				 "  -h, --help   Display this help\n"
+				 "  -d, --debug  Show debug messages\n" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
 
-	if(argc < 2) {
+	static struct option long_options[] = {
+		{"help", no_argument, NULL, 'h'},
+		{"debug", no_argument, NULL, 'd'},
+	};
+
+	char opt;
+
+	while((opt = getopt_long(argc, argv, "hd", long_options, NULL)) != -1) {
+		switch(opt) {
+			case 'h':
+				printHelp();
+				return 0;
+			case 'd':
+				// TODO
+				break;
+		}
+	}
+
+	if(argv[argc - 1][0] == '-') {
 		std::cout << "Please specify a file to convert" << std::endl;
 		return 1;
 	}
 
-	Midfile midfile(argv[1]);
+	Midfile midfile(argv[argc - 1]);
 	if(!midfile.is_open()) {
-		std::cout << "File " << argv[1] << " could not be opened" << std::endl;
+		std::cout << "File " << argv[argc - 1] << " could not be opened" << std::endl;
 		return 2;
 	}
 
